1fce20256e5d498e1c23fdbab8b793e0a2043a6e
LukaszMoskwaCTW <lukasz.m@ctw.inc>
2023-06-14 10:16:13 +0900

    feat: add theprimeagen config + copilot

--- lua/lykos94/init.lua
+++ lua/lykos94/init.lua
@@ -1,5 +1,10 @@
-require("lykos94.remap") 
+require("lykos94.remap")
+require("lykos94.set")
 vim.cmd([[
 nnoremap Q q
 nnoremap q <Nop>
+
+noremap <silent> <C-S>          :update<CR>
+vnoremap <silent> <C-S>         <C-C>:update<CR>
+inoremap <silent> <C-S>         <C-O>:update<CR>
 ]])
