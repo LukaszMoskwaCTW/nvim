470da7ae53f9b014b645a125129d4c0167d3fb95
LukaszMoskwaCTW <lukasz.m@ctw.inc>
2023-07-08 14:01:10 +0900

    feat: allow seach telescope hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,6 +9,12 @@ require("telescope").setup({
       },
     },
   },
+  pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
 })
 
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
