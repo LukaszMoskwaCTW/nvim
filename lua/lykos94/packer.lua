89fe85197d9586e974773bca02d25a9611f8ed07
LukaszMoskwaCTW <lukasz.m@ctw.inc>
2024-07-29 10:20:04 +0900

    chore: updates

--- lua/lykos94/packer.lua
+++ lua/lykos94/packer.lua
@@ -1,163 +1,164 @@
-vim.cmd.packadd('packer.nvim')
+vim.cmd.packadd("packer.nvim")
 
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"loctvl842/monokai-pro.nvim",
+		config = function()
+			require("monokai-pro").setup({
+				filter = "pro",
+			})
+		end,
+	})
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				"williamboman/mason.nvim",
+			},
+			{
+				"williamboman/mason-lspconfig.nvim",
+			},
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	})
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use {
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-      require("monokai-pro").setup({
-        filter = "pro",
-      })
-    end
-  }
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      {
-        'williamboman/mason.nvim'
-      },
-      {
-        'williamboman/mason-lspconfig.nvim'
-      },
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },     -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'L3MON4D3/LuaSnip' },     -- Required
-    }
-  }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
+	use("nvim-treesitter/nvim-treesitter-context")
+	use("mbbill/undotree")
+	use("preservim/nerdtree")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
+	use("github/copilot.vim")
+	use("prisma/vim-prisma")
+	use("mhinz/vim-signify")
+	use("tpope/vim-commentary")
+	use("tpope/vim-fugitive")
+	use("tpope/vim-surround")
+	use("chentoast/marks.nvim")
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+	-- File explorer icons
+	use("nvim-tree/nvim-web-devicons")
+	-- Git status
+	use("lewis6991/gitsigns.nvim")
+	-- Buffer navigation tabline
+	--
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("null-ls").setup()
+		end,
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	--
+	use("romgrk/barbar.nvim", {
+		config = function()
+			require("bufferline").setup({ animation = false })
+		end,
+	})
+	use("ryanoasis/vim-devicons")
+	use("christoomey/vim-tmux-navigator")
 
-  use('nvim-treesitter/nvim-treesitter-context')
-  use('mbbill/undotree')
-  use('preservim/nerdtree')
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use('github/copilot.vim')
-  use('prisma/vim-prisma')
-  use('mhinz/vim-signify')
-  use('tpope/vim-commentary')
-  use('tpope/vim-fugitive')
-  use('tpope/vim-surround')
-  use('chentoast/marks.nvim')
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
+	use("windwp/nvim-autopairs")
 
-  -- File explorer icons
-  use 'nvim-tree/nvim-web-devicons'
-  -- Git status
-  use 'lewis6991/gitsigns.nvim'
-  -- Buffer navigation tabline
-  --
-  use({
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      require("null-ls").setup()
-    end,
-    requires = { "nvim-lua/plenary.nvim" },
-  })
-  --
-  use('romgrk/barbar.nvim', {
-    config = function()
-      require 'bufferline'.setup { animation = false }
-    end
-  })
-  use 'ryanoasis/vim-devicons'
-  use 'christoomey/vim-tmux-navigator'
+	use({
+		"kawre/leetcode.nvim",
+		requires = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		build = {
+			run = ":TSUpdate html",
+		},
+		config = function()
+			require("leetcode").setup({
+				arg = "l",
+				-- directory = '$HOME/.leetcode',
+				lang = "python3",
+				-- workspace = '/Users/mofiqulislam/leetcode',
+				-- workspace = '/Users/mofiqulislam/leetcode',
+				-- workspace = '/Users/mofiqulislam/leetcode
+			})
+		end,
+	})
 
-  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-    require("toggleterm").setup()
-  end }
-  use "windwp/nvim-autopairs"
+	-- Test
+	use("vim-test/vim-test")
+	use("David-Kunz/jester")
 
-  use {
-    'kawre/leetcode.nvim',
-    requires = {
-      'nvim-telescope/telescope.nvim',
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim'
-    },
-    build = {
-      run = ':TSUpdate html',
-    },
-    config = function()
-      require('leetcode').setup({
-        arg = 'l',
-        -- directory = '$HOME/.leetcode',
-        lang = 'javascript',
-        -- workspace = '/Users/mofiqulislam/leetcode',
-        -- workspace = '/Users/mofiqulislam/leetcode',
-        -- workspace = '/Users/mofiqulislam/leetcode
-      })
-    end,
-  }
+	-- Formatter
+	use("elentok/format-on-save.nvim")
+	use("prettier/vim-prettier")
 
-  -- Test
-  use('vim-test/vim-test')
-  use 'David-Kunz/jester'
+	-- Debugger
+	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+	use({
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+	-- Add support for scala
+	use({ "scalameta/nvim-metals", requires = { "nvim-lua/plenary.nvim" } })
+	use({
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.notify").setup()
+		end,
+	})
 
-  -- Formatter
-  use "elentok/format-on-save.nvim"
-  use('prettier/vim-prettier')
+	-- use('sheerun/vim-polyglot')
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
-  -- Debugger
-  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
-  use {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-  use {
-    "folke/trouble.nvim",
-    requires = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-  -- Add support for scala
-  use({ 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } })
-  use({
-    'echasnovski/mini.nvim',
-    config = function()
-      require("mini.notify").setup()
-    end
-  })
-
-
-  -- use('sheerun/vim-polyglot')
-  use('JoosepAlviste/nvim-ts-context-commentstring')
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-
-  }
-
-  use "$HOME/git/depscope"
+	use("$HOME/git/depscope")
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -121,7 +121,7 @@ _G.packer_plugins = {
     url = "https://github.com/David-Kunz/jester"
   },
   ["leetcode.nvim"] = {
-    config = { "\27LJ\2\nS\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\barg\6l\tlang\15javascript\nsetup\rleetcode\frequire\0" },
+    config = { "\27LJ\2\nP\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\barg\6l\tlang\fpython3\nsetup\rleetcode\frequire\0" },
     loaded = true,
     path = "/Users/ctw/.local/share/nvim/site/pack/packer/start/leetcode.nvim",
     url = "https://github.com/kawre/leetcode.nvim"
@@ -315,38 +315,38 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: todo-comments.nvim
-time([[Config for todo-comments.nvim]], true)
-try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18todo-comments\frequire\0", "config", "todo-comments.nvim")
-time([[Config for todo-comments.nvim]], false)
--- Config for: Comment.nvim
-time([[Config for Comment.nvim]], true)
-try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
-time([[Config for Comment.nvim]], false)
--- Config for: leetcode.nvim
-time([[Config for leetcode.nvim]], true)
-try_loadstring("\27LJ\2\nS\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\barg\6l\tlang\15javascript\nsetup\rleetcode\frequire\0", "config", "leetcode.nvim")
-time([[Config for leetcode.nvim]], false)
--- Config for: trouble.nvim
-time([[Config for trouble.nvim]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
-time([[Config for trouble.nvim]], false)
--- Config for: toggleterm.nvim
-time([[Config for toggleterm.nvim]], true)
-try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
-time([[Config for toggleterm.nvim]], false)
--- Config for: monokai-pro.nvim
-time([[Config for monokai-pro.nvim]], true)
-try_loadstring("\27LJ\2\nK\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\vfilter\bpro\nsetup\16monokai-pro\frequire\0", "config", "monokai-pro.nvim")
-time([[Config for monokai-pro.nvim]], false)
 -- Config for: mini.nvim
 time([[Config for mini.nvim]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\16mini.notify\frequire\0", "config", "mini.nvim")
 time([[Config for mini.nvim]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
+-- Config for: Comment.nvim
+time([[Config for Comment.nvim]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+time([[Config for Comment.nvim]], false)
+-- Config for: trouble.nvim
+time([[Config for trouble.nvim]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+time([[Config for trouble.nvim]], false)
+-- Config for: monokai-pro.nvim
+time([[Config for monokai-pro.nvim]], true)
+try_loadstring("\27LJ\2\nK\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\vfilter\bpro\nsetup\16monokai-pro\frequire\0", "config", "monokai-pro.nvim")
+time([[Config for monokai-pro.nvim]], false)
+-- Config for: leetcode.nvim
+time([[Config for leetcode.nvim]], true)
+try_loadstring("\27LJ\2\nP\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\barg\6l\tlang\fpython3\nsetup\rleetcode\frequire\0", "config", "leetcode.nvim")
+time([[Config for leetcode.nvim]], false)
 -- Config for: null-ls.nvim
 time([[Config for null-ls.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fnull-ls\frequire\0", "config", "null-ls.nvim")
 time([[Config for null-ls.nvim]], false)
+-- Config for: todo-comments.nvim
+time([[Config for todo-comments.nvim]], true)
+try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18todo-comments\frequire\0", "config", "todo-comments.nvim")
+time([[Config for todo-comments.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
