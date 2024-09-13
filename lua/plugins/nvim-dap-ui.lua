eb579c52d47ccfa0df2f6220ab0a57b9480e7790
LukaszMoskwaCTW <lukasz.m@ctw.inc>
2024-09-13 18:53:11 +0900

    chore: re-add dap

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -19,18 +19,18 @@ return {
 			layouts = {
 				{
 					elements = {
-						-- { id = "stacks", size = 0.5 },
-						-- { id = "scopes", size = 0.25 },
-						{ id = "breakpoints", size = 1 },
-						-- { id = "watches", size = 0.25 },
+						{ id = "stacks", size = 0.5 },
+						{ id = "scopes", size = 0.25 },
+						{ id = "breakpoints", size = 0.25 },
+						{ id = "watches", size = 0.25 },
 					},
 					position = "right",
-					size = 60,
+					size = 100,
 				},
 				{
 					elements = {
-						-- { id = "repl", size = 0.35 },
-						{ id = "console", size = 1 },
+						{ id = "repl", size = 0.35 },
+						{ id = "console", size = 0.65 },
 					},
 					position = "bottom",
 					size = 14,
