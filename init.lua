caf9296d921572f27bd7173424df8de633c85323
LukaszMoskwaCTW <lukasz.m@ctw.inc>
2024-10-29 14:31:05 +0900

    feat: allow autoclose lazygit when quitting

--- init.lua
+++ init.lua
@@ -10,3 +10,19 @@ vim.treesitter.language.register("markdown", "octo")
 -- autocmd TermOpen * setlocal nonumber norelativenumber
 -- autocmd TermOpen * startinsert
 vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber")
+
+-- Create an autocommand group for lazygit terminal close behavior
+vim.api.nvim_create_augroup("LazygitAutoClose", { clear = true })
+
+-- Define the autocommand to close the terminal when lazygit exits
+vim.api.nvim_create_autocmd("TermClose", {
+	group = "LazygitAutoClose",
+	pattern = "*",
+	callback = function(event)
+		-- Get the buffer name and check if it contains 'lazygit'
+		local bufname = vim.api.nvim_buf_get_name(event.buf)
+		if bufname:match("lazygit") then
+			vim.api.nvim_buf_delete(event.buf, { force = true })
+		end
+	end,
+})
